3368c85d59c7cf5ec6d636ac3bea05aa98b467f0
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-07-10 11:36:32 +0200

    Update for publish

--- src/components/componentHtmlImport.ts
+++
@@ -1,9 +0,0 @@
-// components.ts
-export { Layout } from './global/layout/layout';
-export { Window } from './global/window/window';
-export { Menu } from './global/menu/menu';
-export { AppIcon } from './global/appIcon/appIcon';
-export { File } from './global/file/file';
-export { OpenedApps } from './global/openedApps/openedApps';
-export { Dock } from './global/dock/dock';
-export {Desktop} from './global/desktop/desktop';
